bf7720602f410efefa9cd186f0f1411dc2a5e6d9
martins <martinsmaurojr@hotmail.com>
2023-05-18 22:26:26 -0300

    Initial Commit Angular

--- casadeFamilia(ATUALIZADO)/casadeFamilia/casadeFamilia/src/main/java/gerenciador/casadeFamilia/casadeFamilia/repository/FuncionariaRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package gerenciador.casadeFamilia.casadeFamilia.repository;
-
-import gerenciador.casadeFamilia.casadeFamilia.model.Funcionaria;
-
-import java.util.List;
-
-public interface FuncionariaRepositoryCustom {
-    List<Funcionaria> localizarPorFuncionaria(Funcionaria funcionaria);
-}
